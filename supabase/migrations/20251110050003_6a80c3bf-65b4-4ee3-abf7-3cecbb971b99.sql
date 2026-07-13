ddb4e6283dc1dad89d883f6110ea1fd47199f3b9
Boom <tnsduq1324@naver.com>
2026-07-13 20:55:15 +0900

    Remove all repository files

--- supabase/migrations/20251110050003_6a80c3bf-65b4-4ee3-abf7-3cecbb971b99.sql
+++
@@ -1,6 +0,0 @@
--- Add foreign key from bids.user_id to profiles.id
-ALTER TABLE public.bids
-ADD CONSTRAINT bids_user_id_fkey 
-FOREIGN KEY (user_id) 
-REFERENCES public.profiles(id) 
-ON DELETE CASCADE;
